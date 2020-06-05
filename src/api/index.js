a0f18a2be8cf64230aa64b675330cb2c1dc91be2
robinshins <robinshins674@gmail.com>
2020-06-06 01:21:27 +0900

    change url

--- src/api/index.js
+++ src/api/index.js
@@ -8,8 +8,8 @@ const config = {
   };
 
 export default axios.create({
-  baseURL: 'http://35.238.162.67/',
-  //baseURL: 'https://www.bearbear.co.kr/',
+  //baseURL: 'http://35.238.162.67/',
+  baseURL: 'https://www.bearbear.co.kr/',
     headers: config,
     withCredentials: true,
 
